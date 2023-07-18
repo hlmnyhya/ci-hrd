ba81425ef95bb915d113145e67d79a939839b378
Hilmanadi Yahya <hlmnyhya99@gmail.com>
2023-07-18 14:29:02 +0800

    dev version

--- application/controllers/Mpp/MPP.php
+++ application/controllers/Mpp/MPP.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class MPP extends CI_Controller {
+class Mpp extends CI_Controller {
 
     public function __construct()
     {
