cd14f5d36f51b20c2bf957e72eb0efad6fe1771d
Hilmanadi Yahya <hlmnyhya99@gmail.com>
2023-07-18 14:07:57 +0800

    beta version

--- application/models/mpp/m_mpp.php
+++ application/models/mpp/m_mpp.php
@@ -1,7 +1,7 @@
 <?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
     
-class M_Mpp extends CI_Model{
+class M_MPPp extends CI_Model{
     
     public function show_data()
     {
